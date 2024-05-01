f82705be2a9c04798200f1e6f77483e0514e419f
AtoAidoo <80718069+AtoAidoo@users.noreply.github.com>
2024-05-01 14:13:22 +0000

    Add files via upload
    I have added the backed aspect the sign up

--- SignUpForm.tsx
+++ SignUpForm.tsx
@@ -1,39 +1,73 @@
 import { useContext, useState } from "react";
+
+import axios from "axios";
+
+import { PersonObject } from "react-chat-engine-advanced";
+
+import { useIsMobile } from "../functions/isMobile";
+import { Context } from "../functions/context";
+import { privateKey } from "../functions/constants";
+
 import TextInput from "./components/TextInput";
 import PhotoInput from "./components/PhotoInput";
 import Button from "./components/Button";
 import Link from "./components/Link";
 
 interface SignUpFormProps {
-    onHasAccount: () => void;
-  }
+  onHasAccount: () => void;
+}
 
 const SignUpForm = (props: SignUpFormProps) => {
-    // State
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [avatar, setAvatar] = useState<File | undefined>(undefined);
-    // Hooks
-    const { setUser } = useContext(Context);
-    const isMobile: boolean = useIsMobile();
-  
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-  
-      const userJson: PersonObject = {
-        email: email,
-        username: email,
-        first_name: firstName,
-        last_name: lastName,
-        secret: password,
-        avatar: null,
-        custom_json: {},
-        is_online: true,
-      };
+  // State
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState<File | undefined>(undefined);
+  // Hooks
+  const { setUser } = useContext(Context);
+  const isMobile: boolean = useIsMobile();
 
-return (
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const userJson: PersonObject = {
+      email: email,
+      username: email,
+      first_name: firstName,
+      last_name: lastName,
+      secret: password,
+      avatar: null,
+      custom_json: {},
+      is_online: true,
+    };
+
+    let formData = new FormData();
+    formData.append("email", email);
+    formData.append("username", email);
+    formData.append("first_name", firstName);
+    formData.append("last_name", lastName);
+    formData.append("secret", password);
+    if (avatar) {
+      formData.append("avatar", avatar, avatar.name);
+    }
+
+    const headers = { "Private-Key": privateKey };
+
+    axios
+      .post("https://api.chatengine.io/users/", formData, {
+        headers,
+      })
+      .then((r) => {
+        if (r.status === 201) {
+          userJson.avatar = r.data.avatar;
+          setUser(userJson);
+        }
+      })
+      .catch((e) => console.log("Error", e));
+  };
+
+  return (
     <div>
       <div className="form-title">Create an account</div>
 
